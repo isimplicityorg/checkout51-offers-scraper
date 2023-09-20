d397d6025da4e94043f04d9b0b61a4404661652a
Brian Caudill <brian.m.caudill@gmail.com>
2023-09-20 15:16:57 -0300

    Update code.js
    updated to get all of the offers and separate with "---------"

--- code.js
+++ code.js
@@ -1,12 +1,13 @@
-// Get the <li> element that contains the offer information
-const offerElement = document.querySelector('li.offer');
-console.log(offerElement)
-if (offerElement) {
-  // Select the <div class="info"> element
-  const infoElement = document.querySelector('.info');
+// Get all <li> elements that contain offer information
+const offerElements = document.querySelectorAll('li.offer');
+
+// Iterate through each offer element
+offerElements.forEach((offerElement, index) => {
+  // Select the <div class="info"> element within the current offer element
+  const infoElement = offerElement.querySelector('.info');
 
   if (infoElement) {
-    // Select <span> elements with specific classes
+    // Select <span> elements with specific classes within the info element
     const nameSpan = infoElement.querySelector('span.name.ng-binding');
     const descriptionSpan = infoElement.querySelector('span.description.ng-binding.ng-scope');
     const amountSpan = offerElement.querySelector('div.cb.ng-scope span.amount.ng-binding');
@@ -16,14 +17,13 @@ if (offerElement) {
     const description = descriptionSpan ? descriptionSpan.textContent.trim() : '';
     const amount = amountSpan ? amountSpan.textContent.trim() : '';
 
-    // Log the extracted information
+    // Log the extracted information for the current offer
+    console.log(`Offer ${index + 1}:`);
     console.log('Name:', name);
     console.log('Description:', description);
     console.log('Amount:', amount);
+    console.log('-------------------------');
   } else {
-    console.log('Info element not found');
+    console.log(`Info element not found for Offer ${index + 1}`);
   }
-} else {
-  console.log('Offer element not found');
-}
-
+});
