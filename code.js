cc9ce336c4c9b6936e1183f9a140ae6bb40ef3ed
Brian Caudill <brian.m.caudill@gmail.com>
2023-09-20 15:11:44 -0300

    Update code.js
    parsing out Name: Buy 2: Ricola debugger eval code:20:13
    Description: Valid on 19ct-45ct size bags. Items must appear on the same receipt. debugger eval code:21:13
    Amount: <empty string> from one offer

--- code.js
+++ code.js
@@ -1,1 +1,29 @@
+// Get the <li> element that contains the offer information
+const offerElement = document.querySelector('li.offer');
+console.log(offerElement)
+if (offerElement) {
+  // Select the <div class="info"> element
+  const infoElement = document.querySelector('.info');
+
+  if (infoElement) {
+    // Select <span> elements with specific classes
+    const nameSpan = infoElement.querySelector('span.name.ng-binding');
+    const descriptionSpan = infoElement.querySelector('span.description.ng-binding.ng-scope');
+    const amountSpan = offerElement.querySelector('div.cb.ng-scope span.amount.ng-binding');
+
+    // Extract the text content from the selected <span> elements
+    const name = nameSpan ? nameSpan.textContent.trim() : '';
+    const description = descriptionSpan ? descriptionSpan.textContent.trim() : '';
+    const amount = amountSpan ? amountSpan.textContent.trim() : '';
+
+    // Log the extracted information
+    console.log('Name:', name);
+    console.log('Description:', description);
+    console.log('Amount:', amount);
+  } else {
+    console.log('Info element not found');
+  }
+} else {
+  console.log('Offer element not found');
+}
 
